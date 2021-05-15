83635ae36458db0b3956fe3404d430ff844aae23
PairedPrototype <pairedprototype@hotmail.com>
2021-05-15 01:47:30 +0100

    Loads, parses and returns the active miner count

--- index.ts
+++ index.ts
@@ -1,14 +1,56 @@
 #!/usr/bin/env node
-import yargs = require('yargs/yargs');
-import helpers = require('yargs/helpers');
-import { argv } from 'yargs';
+import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-yargs(helpers.hideBin(process.argv))
-    .option('url', {
-        alias: 'u',
-        type: 'string',
-        description: 'URL to parse the worker status from',
+let loggingEnabled = false;
+
+function doLogThings(message: string) {
+    if (loggingEnabled) {
+        console.log(message);
+    }
+}
+
+async function getStatus(url: string) {
+    doLogThings('launching browser');
+    const browser = await puppeteer.launch();
+
+    doLogThings('creating new browser page');
+    const page = await browser.newPage();
+
+    doLogThings('browser navigating');
+    await page.goto(url);
+
+    doLogThings('fetching page content');
+    const pageContent = await page.content();
+
+    doLogThings('parsing page content');
+    const $ = cheerio.load(pageContent);
+
+    doLogThings('Finding element');
+    const el = $('.stats .workers .card-body .inactive');
+    doLogThings('Amount of inactive workers:');
+
+    console.log(el.text());
+
+    doLogThings('closing browser');
+    await browser.close();
+}
+
+yargs(hideBin(process.argv))
+    .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging'
     })
+    .command(
+        'get <url>',
+        'get the worker status from <url>',
+        (yargs) => yargs.positional('url', { describe: 'url to get status from' }),
+        async (argv) => {
+            loggingEnabled = argv.verbose ?? false;
+            await getStatus(argv.url as string);
+        },
+    )
     .argv;
-
-console.log(argv);
